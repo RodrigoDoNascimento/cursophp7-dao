2e1d11b20906c35aa9915f1af6b2b80266c939bb
RodrigoDoNascimento <rodrigoguigui@gmail.com>
2019-12-20 19:49:52 -0300

    Método lista

--- index.php
+++ index.php
@@ -2,10 +2,24 @@
 
 require_once("config.php");
 
-$root = new Usuario();
-
-$root->loadById(3);
-
+// Carrega um usuário
+/* $root = new Usuario();
+$root->loadById(3); 
 echo $root;
+*/
+
+// Carrega uma lista de usuários
+/* $lista = Usuario::getList();
+echo json_encode($lista); */
+
+// Carrega uma lista de usuários buscando pelo login
+/* $search = Usuario::search("e");
+echo json_encode($search); */
+
+// Carrega um usuário usando o login e a senha
+$usurario = new Usuario();
+$usurario->login("rodrigo", "197538");
+echo $usurario;
+
 
 ?>
